d8a15d7f0354a89ce816b87bf5c0bae300ea4611
Yeliazar <erik.ruzhakov@mail.ru>
2021-09-14 11:45:25 +0300

    Created component Announcement and Slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from "../components/navbar/Navbar";
+import Announcement from "../components/Announcement/Announcement";
+import Slider from "../components/Slider/Slider";
 
 const Home = () => {
     return (
         <div className="container">
+            <Announcement/>
             <Navbar/>
+            <Slider/>
         </div>
     )
 }
